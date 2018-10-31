9e90db79834f70a36e16fac45116a1249f1a7295
Ablaev Lyuman <dracos@list.ru>
2018-10-31 20:52:45 +0300

    PW Images

--- PW/PW.R
+++ PW/PW.R
@@ -1,105 +1,104 @@
-euclideanDistance <- function(u, v)
+euclideanDistance = function(u, v)
 {
-
   return (sqrt(sum((u - v)^2)))
-
+}
+kernelEP = function(r){
+  return ((3/4*(1-r^2)*(abs(r)<=1)))
+}
+kernelR = function(r){
+  return ((0.5 * (abs(r) <= 1) ))
+} 
+kernelT = function(r){
+  return ((1 - abs(r)) * (abs(r) <= 1)) 
+}
+kernelQ = function(r){
+  return ((15 / 16) * (1 - r ^ 2) ^ 2 * (abs(r) <= 1)) 
+}
+kernelG = function(r){
+  return (((2*pi)^(-1/2)) * exp(-1/2*r^2))
 }
 
-kernelEP = function(x,y,h,metricFunction = euclideanDistance){
- 
-   r = metricFunction(x,y)
-    if(r<=h){
-      return(3/4*(1-r^2))
-    }
-
-    return(0)
-  
-}
 
 
-
-Parzen <- function(y,h)
+Parzen = function(XL,y,h,metricFunction = euclideanDistance)
 {
   n = dim(xl)[1]
   weights = rep(0,3)
-  names(weights) <- c("setosa", "versicolor", "virginica")
+  names(weights) = c("setosa", "versicolor", "virginica")
   for(i in 1:n)
   {
-    x<-xl[i,1:2]
-    class<-xl[i,3]
-    weights[class]<-kernelEP(x,y,h)+weights[class];
+    
+    x=XL[i,1:2]
+    class=XL[i,3]
+    
+    r = metricFunction(x,y)/h
+    weights[class]=kernelG(r)+weights[class];
   }
-
-  class <- names(which.max(weights))
+  
+  class = names(which.max(weights))
+  
   if(max(weights)==0){
-    return ("grey")
+    return ("NA")
   }
   else{
     return (class)
   }
-  
-
- 
-
-  
-
 }
+
+plotWindows = function(h){
+  for(i in seq(0, 7, 0.1)){
+    for(j in seq(0,3,0.1)){
+      z = c(i, j)
+      class = Parzen(xl,z,h)
+      if(class!="NA"){
+        points(z[1], z[2], pch = 1,col=colors[class])
+      }
+    }
+  }
+}
+
 
 LOO = function(xl,class) {
   n = dim(xl)[1]
-  loo = rep(0, 15)
+  loo = rep(0, 20)
   
   for(i in 1:(n)){
-
     u=xl[i, 1:2]
-
-    for(h in 1:15){
-      H <- h/5;
-      test=Parzen(u,H)
-
+    v=xl[-i,1:3]
+    for(h in 1:20){
+      H = h/5;
+      test=Parzen(v,u,H)
       if(colors[test] != colors[class[i]]){
+        
         loo[h] = loo[h]+1;
+        
       }    
     } 
   }
- 
-  loo = loo / 15
-  x = seq(0.2,3,0.2)
-  y = loo
-  plot(x, y,main ="LOO for PW(H)", xlab="h", ylab="LOO", type = "l")
   
+  loo = loo / n
+  x = seq(0.1,2,0.1)
+  plot(x, loo,main ="LOO for PW(H)", xlab="h", ylab="LOO", type = "l")
   min=which.min(loo)
   lOOmin=round(loo[min],3)
   xmin=min*3
-  minX=min/5
+  minX=min/10
   points(minX, loo[min], pch = 21, col = "red",bg = "red")
   label = paste("   H = ", minX, "\n", "   LOO = ", lOOmin, sep = "")
   text(minX, lOOmin, labels = label, pos=4, col = "red")
-  print("*******************************************************************")
   
-  text <- paste("Map classificaton for PW with h = ", minX)
-  plot(iris[X, 3:4],main=text, pch = 21, bg = colors[xl$Species], col = colors[xl$Species],asp='1')
   
-  for(i in seq(0, 7, 0.1)){
-    for(j in seq(0,3,0.1)){
-      z <- c(i, j)
-
-      class <- Parzen(z,minX)
-   
-      points(z[1], z[2], pch = 1, bg = colors[class],col=colors[class])
-      
-    }
-  }
-
+  text = paste("Map classificaton for PW(Gaussian) with h = ", minX)
+  plot(iris[, 3:4],main=text, pch = 21, bg = colors[xl$Species], col = colors[xl$Species],asp='1')
+  plotWindows(minX)
 }
 
 
-par(mfrow = c(1, 2))
 
-colors <- c("setosa" = "red", "versicolor" = "green", "virginica" = "blue","grey"="grey")
-X<-sample(c(1:150),60,replace=TRUE) 
-xl <- iris[X, 3:5] 
-class <- iris[X, 5]
+par(mfrow = c(1, 2))
+colors = c("setosa" = "red", "versicolor" = "green", "virginica" = "blue", "NA" = "NA")
+xl = iris[, 3:5] 
+class = iris[, 5]
 LOO(xl,class)
 
 
