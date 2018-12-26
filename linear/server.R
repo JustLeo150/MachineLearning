59f2d223dc62462761f07a8e3ed504e1fb814b82
Ablaev Lyuman <dracos@list.ru>
2018-12-27 00:54:58 +0300

    linears and reamde

---
+++ linear/server.R
@@ -0,0 +1,212 @@
+library(MASS)
+library("gamlss")
+# Нормализация обучающей выборки
+normalize = function(xl) {
+  cols = dim(xl)[2]
+  
+  for (i in 1:cols) {
+    xl[, i] = (xl[, i] - min(xl[, i])) / (max(xl[, i]) - min(xl[, i]))
+  }
+  return(xl)
+}
+
+# Добавляем w0 = -1 как третью колонку
+prepare = function(xl) {
+  rows = dim(xl)[1]
+  cols = dim(xl)[2]
+  xl = cbind(xl, rep(-1, rows))
+}
+
+
+# Функции потерь
+adaline = function(x) {
+  (x - 1) ^ 2
+}
+
+perceptron = function(x) {
+  max(-x, 0)
+}
+
+regression = function(x) {
+  log2(1 + exp(-x))
+}
+
+
+adalineW = function(w, eta, xi, yi) {
+  w - eta * (sum(w * xi) - yi) * xi
+}
+
+perceptronW = function(w, eta, xi, yi) {
+  w + eta * yi * xi
+}
+
+regressionW = function(w, eta, xi, yi) {
+  sigmoid = function(z) {
+    1 / (1 + exp(-z))
+  }
+  
+  w + eta * xi * yi * sigmoid(-sum(w * xi) * yi)
+}
+
+server = function(input, output) {
+  getData = function() {
+    n1 = input$n1
+    n2 = input$n2
+    
+    covar1 = matrix(c(input$cov1X, 0, 0, input$cov1Y), 2, 2)
+    covar2 = matrix(c(input$cov2X, 0, 0, input$cov2Y), 2, 2)
+    mu1 = c(input$mu1X, input$mu1Y)
+    mu2 = c(input$mu2X, input$mu2Y)
+    x1 = mvrnorm(n1, mu1, covar1)
+    x2 = mvrnorm(n2, mu2, covar2)
+    
+    list("x1" = x1, "x2" = x2)
+
+  }
+
+
+
+    
+
+  
+  # Стохастический градиент
+  stoh = function(xl, classes, L, updateRule, iterId) {
+    #изначальная настройка алгоритма
+    rows = dim(xl)[1]
+    cols = dim(xl)[2]
+    w = runif(cols, -1 / (2 * cols), 1 / (2 * cols))
+    lambda = 1 / rows
+    
+    # начальное Q
+    Q = 0
+    for (i in 1:rows) {
+      margin = sum(w * xl[i,]) * classes[i]
+      Q = Q + L(margin)
+    }
+    Q.prev = Q
+    
+    iter = 0
+    repeat {
+      iter = iter + 1
+      
+      # выбрать объекты с ошибкой
+      margins = rep(0, rows)
+      for (i in 1:rows) {
+        xi = xl[i,]
+        yi = classes[i]
+        margins[i] = sum(w * xi) * yi
+      }
+      errorIndecies = which(margins <= 0)
+      
+      #выходим, если выборки полностью разделены
+      if (length(errorIndecies) == 0) {
+        break;
+      }
+      
+      # выбираем случайный ошибочный объект          
+      i = sample(errorIndecies, 1)
+      xi = xl[i,]
+      yi = classes[i]
+      
+      # высчитываем ошибку
+      margin = sum(w * xi) * yi
+      error = L(margin)
+      
+      # обновляем веса
+      eta = 1 / iter
+      w = updateRule(w, eta, xi, yi)
+      
+      # новое Q
+      Q = (1 - lambda) * Q + lambda * error
+      
+      # выходим, если Q стабилизировалось
+      if (abs(Q.prev - Q) / abs(max(Q.prev, Q)) < 1e-5)
+        break;
+      
+      # выходим, если слишком много итераций
+      if (iter == 20000)
+        break;
+      
+      Q.prev = Q
+    }
+    
+    # выводим количество итераций
+    output[[iterId]] = renderText({
+      paste0(iter)
+    })
+    
+    return(w)
+  }
+  
+  drawLine = function(w, color) {
+    abline(a = w[3] / w[2], b = -w[1] / w[2], lwd = 2, col = color)
+  }
+  
+  output$plot = renderPlot({
+    #Создаем тестовые данные
+    output[["adaline"]] = renderText({
+      paste0()
+    })
+    output[["perseptron"]] = renderText({
+      paste0()
+    })
+    output[["regression"]] = renderText({
+      paste0()
+    })
+
+   
+   print(input$mu1X)
+    
+    x.data <- rNO(input$n1,input$mu1X,input$cov1X)
+    print(x.data)
+    y.data <- rNO(input$n1,input$mu1Y,input$cov1Y)
+   
+    
+    x2.data <- rNO(input$n2,input$mu2X,input$cov2X)
+    y2.data <- rNO(input$n2,input$mu2Y,input$cov2Y)
+    
+    
+    x1 <- cbind(x.data,y.data)
+    x2 <- cbind(x2.data,y2.data)
+    
+    
+    y1 = rep(-1, input$n1)
+    y2 = rep(+1, input$n2)
+    
+    xl = rbind(x1, x2)
+    classes = c(y1, y2)
+    
+    xl1 = xl[classes == -1,]
+    xl2 = xl[classes == 1,]
+    xl = rbind(xl1, xl2)
+    colors = c(rep("red", dim(xl1)[1]), rep("blue", dim(xl2)[1]))
+    
+    plot(xl[, ], pch = 21, bg = colors, col="black",asp = 1, xlab = "X", ylab = "Y")
+    if(input$A | input$P | input$R)
+      legend("bottomleft", legend=c(if(input$A)"adaline", if(input$P)"perseptron", if(input$R)"regression"),
+             col=c(if(input$A)"brown", if(input$P)"green", if(input$R)"orange"), lty=1, cex=1.5)
+    
+    
+    # Нормализация данных
+     xl = normalize(xl)
+     xl = prepare(xl)
+    
+
+    print(input$A)
+    # Поиск разделяющей поверхности
+    if(input$A==TRUE)
+    w1 = stoh(xl, classes, adaline, adalineW, iterId = "adaline")
+    if(input$P==TRUE)
+    w2 = stoh(xl, classes, perceptron, perceptronW, iterId = "perseptron")
+    if(input$R==TRUE)
+    w3 = stoh(xl, classes, regression, regressionW, iterId = "regression")
+    
+    # Рисуем разделяющую поверхность
+    if(input$A==TRUE)
+    drawLine(w1, "brown")
+    if(input$P==TRUE)
+    drawLine(w2, "green")
+    if(input$R==TRUE)
+    drawLine(w3, "orange")
+  })
+}
---
+++ linear/ui.R
@@ -0,0 +1,116 @@
+nameWidth = 3
+inputWidth = 9
+
+makeSlider = function(id, min, max, value) {
+  sliderInput(
+    inputId = id,
+    label = NULL,
+    min = min,
+    max = max,
+    value = value
+  )
+}
+
+makeCountRow = function(id, min, max, value) {
+  fluidRow(
+    column(nameWidth, h5("Количество")),
+    column(inputWidth, makeSlider(id, min, max, value))
+  )
+}
+
+makeCovMatrixRow = function(id, min, max, values) {
+  fluidRow(
+    column(nameWidth, h5("Дисперсия")),
+    column(inputWidth,
+           fluidRow(
+             column(6, makeSlider(paste0(id, "X"), min, max, values[1])),
+             column(6, makeSlider(paste0(id, "Y"), min, max, values[2]))
+           )
+    )
+  )
+}
+
+makeExpectedRow = function(id, min, max, values) {
+  fluidRow(
+    column(nameWidth, h5("Центр X,Y")),
+    column(inputWidth,
+           fluidRow(
+             column(6, makeSlider(paste0(id, "X"), min, max, values[1])),
+             column(6, makeSlider(paste0(id, "Y"), min, max, values[2]))
+           )
+    )
+  )
+}
+
+makeColoredText = function(text, color) {
+  h5(text, style = paste0("text-align: center; color: ", color))
+}
+
+makeColoredOutput = function(id, color) {
+  span(textOutput(id), style = paste0("text-align: center; color: ", color))
+}
+
+ui <- fluidPage(
+  
+
+  titlePanel("Линейные алгоритмы"),
+
+  fluidRow(
+    column(11),
+    column(2,checkboxInput("A", "adaline", TRUE)),
+
+    column(2,checkboxInput("P", "perseptron", FALSE)),
+
+    column(1,checkboxInput("R", "regression", FALSE))
+),
+fluidRow( column(1),
+  column(nameWidth, h4("Количество итераций")),
+  column(inputWidth,
+         fluidRow(
+           column(2, makeColoredText("Adaline", "brown")),
+           column(2, makeColoredText("perseptron", "green")),
+           column(2, makeColoredText("regression", "orange")),
+           column(12),
+           column(2, makeColoredOutput("adaline", "brown")),
+           column(2, makeColoredOutput("perseptron", "green")),
+           column(2, makeColoredOutput("regression", "orange"))
+         )
+  )
+
+),
+
+  sidebarLayout(
+    
+    sidebarPanel(
+      
+      fluidRow(
+        column(nameWidth),
+        column(inputWidth, h2("Класс Красные", style = "color: red; text-align: center; margin-top: 0"))
+      ),
+      
+      makeCountRow("n1", 10, 200, 50),
+      makeCovMatrixRow("cov1", 1, 20, c(5, 5)),
+      makeExpectedRow("mu1", -50, 50, c(-50, 0)),
+      
+      fluidRow(
+        column(nameWidth),
+        column(inputWidth, h2("Класс Синие", style = "color: blue; text-align: center; margin-top: 0"))
+      ),
+      
+      makeCountRow("n2", 10, 200, 50),
+      makeCovMatrixRow("cov2", 1, 20, c(5, 5)),
+      makeExpectedRow("mu2", -50, 50, c(50, 0))
+
+      
+    ),
+    
+    mainPanel(
+      
+      plotOutput(outputId = "plot", height = "600px")
+      
+    )
+    
+  )
+  
+
+)
